9c04a15bdc4461688df1592b760cbe0884c42965
KotteJR <aleksandar.kotevski@adamass.se>
2025-10-30 17:17:57 +0100

    hello

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/app',
-        destination: '/landingpage',
-        permanent: false,
-      },
-    ];
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.lottie$/,
---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,19 @@
+import Header from '../components/header'
+import Hero from './hero'
+import About from './abouttext'
+import FocusShowcase from './FocusShowcase'
+import Contact from './contact'
+import Footer from '../components/footer'
+
+export default function LandingPageRoute() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <FocusShowcase />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
